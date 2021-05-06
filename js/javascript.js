6a0a5b8a1e845e2b55dd411b71865fc95018c1db
Luis Jose Mejia Ramos <ljmr9807@gmail.com>
2021-05-06 01:32:16 -0500

    Solucion parte de Habilidades

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,20 @@
-var habilidades ="<h2 class='col-12 text-center'>Habilidades</h2>"+
-"<div class='col-xl-12'><spam class='col-xl-3 col-lg-3 text-center'>HTML: </spam>"+
-"<meter class='col-xl-9 col-lg-9' value='0.2'></meter> <div/>";
-
+var habilidades ="<h2 class='col-12 text-center'>Habilidades</h2>";
+let misHabilidades=[new Object({habilidad:'HTML',nivel:0.2}),new Object({habilidad:'CSS',nivel:0.1}),new Object({habilidad:'JavaScript',nivel:0.1}),new Object({habilidad:'Git',nivel:0.1}),new Object({habilidad:'Java',nivel:0.2})];
+misHabilidades.forEach(function(elemento) {
+  habilidades+="<div class='row'>"+
+  "<div class='col-xl-12'>"+
+    "<spam class='col-xl-3 col-lg-3 text-center'>"+elemento.habilidad+": </spam>"+
+    "<meter class='col-xl-9 col-lg-9' value='"+elemento.nivel+"'></meter>"+
+  "</div>"+
+"</div>";
+})
 
 $(document).ready(function(){
     $(".Principal").click(function(){
       $(".img-container").html("<div class='img-container d-flex-justify-content-text text-center'> <img src='img/mifoto.jpg' alt=''> </div>");
     });
-    $(".Habilidades").click(function(){
-        alert("Estas son las habilidades");
+    $(".Habilidades").click(function(){     
+      alert("Estas son las habilidades");
         $(".img-container").html(habilidades);
       });
   });
